86c4d65aa118ecd41a530918005bc94d2364ebea
Manikanta <mani.iiit123@gmail.com>
2015-03-10 04:23:46 +0530

    testing done

--- SharedMemory/shm_msgclient.c
+++ SharedMemory/shm_msgclient.c
@@ -26,7 +26,7 @@
 
 /* Posix IPC object name [system dependant] - see
 http://mij.oltrelinux.com/devel/unixprg/index2.html#ipc__posix_objects */
-#define SHMOBJ_PATH         "./temp2"      
+#define SHMOBJ_PATH         "./tem17"      
 /* maximum length of the content of the message */
 #define MAX_MSG_LENGTH      50
 /* how many types of messages we recognize (fantasy) */
@@ -34,15 +34,20 @@ http://mij.oltrelinux.com/devel/unixprg/index2.html#ipc__posix_objects */
 
 /* message structure for messages in the shared segment */
 struct msg_s {
-    int type;
-    char content[MAX_MSG_LENGTH];
+	int node1;
+	int node2;
+	int cost;
 };
 
 
 int main(int argc, char *argv[]) {
     int shmfd;
-    int shared_seg_size = (1 * sizeof(struct msg_s));   /* want shared segment capable of storing 1 message */
-    struct msg_s *shared_msg;      /* the shared segment, and head of the messages list */
+    int shared_seg_size = (5 * sizeof(struct msg_s));   /* want shared segment capable of storing 1 message */
+    struct msg_s *shared_msg1;      /* the shared segment, and head of the messages list */
+    struct msg_s *shared_msg2;      /* the shared segment, and head of the messages list */
+    struct msg_s *shared_msg3;      /* the shared segment, and head of the messages list */
+    struct msg_s *shared_msg4;      /* the shared segment, and head of the messages list */
+    struct msg_s *shared_msg5;      /* the shared segment, and head of the messages list */
     
 
     /* creating the shared memory object    --  shm_open()  */
@@ -54,15 +59,23 @@ int main(int argc, char *argv[]) {
     printf("Created shared memory object %s\n", SHMOBJ_PATH);
     
     /* requesting the shared segment    --  mmap() */    
-    shared_msg = (struct msg_s *)mmap(NULL, shared_seg_size, PROT_READ | PROT_WRITE, MAP_SHARED, shmfd, 0);
-    if (shared_msg == NULL) {
+    shared_msg1 = (struct msg_s *)mmap(NULL, shared_seg_size/5, PROT_READ | PROT_WRITE, MAP_SHARED, shmfd, 0);
+    shared_msg2 = (struct msg_s *)mmap(NULL, shared_seg_size/5, PROT_READ | PROT_WRITE, MAP_SHARED, shmfd, 0);
+    shared_msg3 = (struct msg_s *)mmap(NULL, shared_seg_size/5, PROT_READ | PROT_WRITE, MAP_SHARED, shmfd, 0);
+    shared_msg4 = (struct msg_s *)mmap(NULL, shared_seg_size/5, PROT_READ | PROT_WRITE, MAP_SHARED, shmfd, 0);
+    shared_msg5 = (struct msg_s *)mmap(NULL, shared_seg_size/5, PROT_READ | PROT_WRITE, MAP_SHARED, shmfd, 0);
+    if (shared_msg1 == NULL) {
         perror("In mmap()");
         exit(1);
     }
     printf("Shared memory segment allocated correctly (%d bytes).\n", shared_seg_size);
 
 
-    printf("Message type is %d, content is: %s\n", shared_msg->type, shared_msg->content);
+    printf("Message type is: node1 =  %d, node2 = %d  cost = %d\n", shared_msg1->node1, shared_msg1->node2, shared_msg1->cost);
+    printf("Message type is: node1 =  %d, node2 = %d  cost = %d\n", shared_msg2->node1, shared_msg2->node2, shared_msg2->cost);
+    printf("Message type is: node1 =  %d, node2 = %d  cost = %d\n", shared_msg3->node1, shared_msg3->node2, shared_msg3->cost);
+    printf("Message type is: node1 =  %d, node2 = %d  cost = %d\n", shared_msg4->node1, shared_msg4->node2, shared_msg4->cost);
+    printf("Message type is: node1 =  %d, node2 = %d  cost = %d\n", shared_msg5->node1, shared_msg5->node2, shared_msg5->cost);
     
     return 0;
 }
--- SharedMemory/shm_msgserver.c
+++ SharedMemory/shm_msgserver.c
@@ -29,7 +29,7 @@
 
 /* Posix IPC object name [system dependant] - see
 http://mij.oltrelinux.com/devel/unixprg/index2.html#ipc__posix_objects */
-#define SHMOBJ_PATH         "./temp2"
+#define SHMOBJ_PATH         "./tem17"
 /* maximum length of the content of the message */
 #define MAX_MSG_LENGTH      50
 /* how many types of messages we recognize (fantasy) */
@@ -37,15 +37,26 @@ http://mij.oltrelinux.com/devel/unixprg/index2.html#ipc__posix_objects */
 
 /* message structure for messages in the shared segment */
 struct msg_s {
-    int type;
-    char content[MAX_MSG_LENGTH];
+    int node1;
+    int node2;
+    int cost;
 };
 
-
+int routesLength=0;
 int main(int argc, char *argv[]) {
+
+	FILE * fp;
+	char * line = NULL;
+	size_t len = 0;
+	ssize_t read;
+
+	fp = fopen("./route", "r");
+	if (fp == NULL)
+		exit(EXIT_FAILURE);
+
+
     int shmfd;
-    int shared_seg_size = (1 * sizeof(struct msg_s));   /* want shared segment capable of storing 1 message */
-    struct msg_s *shared_msg;      /* the shared segment, and head of the messages list */
+    int shared_seg_size = (5 * sizeof(struct msg_s));   /* want shared segment capable of storing 1 message */
     
 
     /* creating the shared memory object    --  shm_open()  */
@@ -59,16 +70,61 @@ int main(int argc, char *argv[]) {
     /* adjusting mapped file size (make room for the whole segment to map)      --  ftruncate() */
     ftruncate(shmfd, shared_seg_size);
 
+    struct msg_s shared_msg[6];      /* the shared segment, and head of the messages list */
+    struct msg_s *shared_msg1;     
+    struct msg_s *shared_msg2;     
+    struct msg_s *shared_msg3;     
+    struct msg_s *shared_msg4;     
+    struct msg_s *shared_msg5;     
     /* requesting the shared segment    --  mmap() */    
-    shared_msg = (struct msg_s *)mmap(NULL, shared_seg_size, PROT_READ | PROT_WRITE, MAP_SHARED, shmfd, 0);
-    if (shared_msg == NULL) {
-        perror("In mmap()");
-        exit(1);
-    }
+    shared_msg1 = (struct msg_s*)mmap(NULL, shared_seg_size/5, PROT_READ | PROT_WRITE, MAP_SHARED, shmfd, 0);
+    shared_msg2 = (struct msg_s*)mmap(NULL, shared_seg_size/5, PROT_READ | PROT_WRITE, MAP_SHARED, shmfd, 0);
+    shared_msg3 = (struct msg_s*)mmap(NULL, shared_seg_size/5, PROT_READ | PROT_WRITE, MAP_SHARED, shmfd, 0);
+    shared_msg4 = (struct msg_s*)mmap(NULL, shared_seg_size/5, PROT_READ | PROT_WRITE, MAP_SHARED, shmfd, 0);
+    shared_msg5 = (struct msg_s*)mmap(NULL, shared_seg_size/5, PROT_READ | PROT_WRITE, MAP_SHARED, shmfd, 0);
     fprintf(stderr, "Shared memory segment allocated correctly (%d bytes).\n", shared_seg_size);
 
-    shared_msg->type = 15;
-    strcpy(shared_msg->content, "This is Manikanta ");
+	int count  = 0;
+	while ((read = getline(&line, &len, fp)) != -1) {
+		if(count==0)
+			routesLength = atoi(line);
+		else{
+					shared_msg[count-1].node1 = line[0]-48;
+					shared_msg[count-1].node2 = line[2]-48;
+					shared_msg[count-1].cost = line[4]-48;
+		}
+		count++;
+	}
+
+
+	shared_msg1->node1 = shared_msg[0].node1;
+	shared_msg1->node2 = shared_msg[0].node2;
+	shared_msg1->cost = shared_msg[0].cost;
+
+	shared_msg2->node1 = shared_msg[1].node1;
+	shared_msg2->node2 = shared_msg[1].node2;
+	shared_msg2->cost = shared_msg[1].cost;
+	
+	shared_msg3->node1 = shared_msg[2].node1;
+	shared_msg3->node2 = shared_msg[2].node2;
+	shared_msg3->cost = shared_msg[2].cost;
+	
+	shared_msg4->node1 = shared_msg[3].node1;
+	shared_msg4->node2 = shared_msg[3].node2;
+	shared_msg4->cost = shared_msg[3].cost;
+	
+	shared_msg5->node1 = shared_msg[4].node1;
+	shared_msg5->node2 = shared_msg[4].node2;
+	shared_msg5->cost = shared_msg[4].cost;
+
+	printf("For 1:  node1=%d node2=%d  cost=%d\n", shared_msg1->node1, shared_msg1->node2, shared_msg1->cost);
+	printf("For 2:  node1=%d node2=%d  cost=%d\n", shared_msg2->node1, shared_msg2->node2, shared_msg2->cost);
+	printf("For 3:  node1=%d node2=%d  cost=%d\n", shared_msg3->node1, shared_msg3->node2, shared_msg3->cost);
+	printf("For 4:  node1=%d node2=%d  cost=%d\n", shared_msg4->node1, shared_msg4->node2, shared_msg4->cost);
+	printf("For 5:  node1=%d node2=%d  cost=%d\n", shared_msg5->node1, shared_msg5->node2, shared_msg5->cost);
+	fclose(fp);
+   // shared_msg->type = 15;
+   // strcpy(shared_msg->content, "This is Manikanta ");
     srandom(time(NULL));
     /* producing a message on the shared segment */
 
